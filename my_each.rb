7c72f2b457cfdd4a8f14858136c2e6f001c5ae74
Jay Fleming <jacobmfleming@gmail.com>
2018-02-21 21:53:35 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,6 @@
-def my_each # put argument(s) here
+def my_each(collection) # put argument(s) here
   # code here
 end
+
+
+my_each(items) { |block_code| puts "Look at this block doing blocky things #{block_code}"}
